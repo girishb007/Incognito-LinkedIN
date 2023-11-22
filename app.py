0155bcbc455980211653c050c6dab52053a398bc
Girish Bisane <girishbisane9096@gmail.com>
2023-11-21 23:50:13 -0800

    Removing webscrapping approach | Using proxycurl

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
-from scraper import scrape_linkedin_profile  # Import the scraping function
-from flask import Flask, request, jsonify, send_file
-
-from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for your entire app
 
-# ...
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/download-profile', methods=['POST'])
-def download_profile():
-    username = request.form['username']
-    profile_data = scrape_linkedin_profile(username)
-    if 'error' in profile_data:
-        return jsonify(profile_data), 400  # Or another appropriate HTTP status code
-    # Further process the scraped data, like generating a PDF
-    pdf_filename = f"{username}_profile.pdf"
+@app.route('/fetch_linkedin', methods=['POST'])
+def fetch_linkedin():
+    linkedin_username = request.form['username']
+    api_key = 'XyDnlOXt3vuGIoRpmX1A-w'
+    headers = {'Authorization': 'Bearer ' + api_key}
+    api_endpoint = 'https://nubela.co/proxycurl/api/v2/linkedin'
+    linkedin_profile_url = f'https://www.linkedin.com/in/{linkedin_username}/'
 
-    # Generate the PDF and save it to a temporary location
-    generate_pdf(profile_data, pdf_filename)
-
-
-    # Send the generated PDF file as a response
-    return send_file(pdf_filename, as_attachment=True)
+    response = requests.get(api_endpoint, params={'url': linkedin_profile_url,'skills': 'include'}, headers=headers)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': 'Failed to fetch data'})
 
 if __name__ == '__main__':
     app.run(debug=True)
