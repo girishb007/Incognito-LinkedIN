a509b69c09214cce05abf2ef5a8f5aad886bda7f
Girish Bisane <girishbisane9096@gmail.com>
2023-11-22 23:26:13 -0800

    pushing server code

---
+++ public/script.js
@@ -0,0 +1,30 @@
+document.getElementById('profileForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+
+    fetch('http://localhost:3009/download-profile', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username: username })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.blob();
+    })
+    .then(blob => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `${username}_linkedin_profile.pdf`;
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const app = express();
+const port = 3009;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.post('/download-profile', async (req, res) => {
+    const username = req.body.username;
+    if (!username) {
+        return res.status(400).send('Username is required');
+    }
+    try {
+        const pdfBuffer = await generateLinkedInPDF(username);
+        res.setHeader('Content-Disposition', `attachment; filename=${username}_linkedin_profile.pdf`);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(pdfBuffer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error generating PDF');
+    }
+});
+
+async function generateLinkedInPDF(username) {
+    const browser = await puppeteer.launch({ headless: "new" }); 
+    //const browser = await puppeteer.launch({ headless: true });
+    //const browser = await puppeteer.launch({ headless: false }); // Set headless: false for debugging
+    const page = await browser.newPage();
+    const linkedinEmail = process.env.LINKEDIN_EMAIL || 'girishbisane9423@gmail.com'; // Replace with your email
+    const linkedinPassword = process.env.LINKEDIN_PASSWORD || 'Hesoyam@9096'; // Replace with your password
+    // Navigate to LinkedIn's login page
+    await page.goto('https://www.linkedin.com/login');
+    // Fill in the login form - make sure to use environment variables or another secure method to store credentials
+    await page.type('#username', 'yourLinkedInEmail', { delay: 30 });
+    await page.type('#password', 'yourLinkedInPassword', { delay: 30 });
+
+    // Click the login button
+    await page.click('[type="submit"]');
+    // Wait for navigation after login
+    await page.waitForNavigation();
+    // Go to the specific user's profile
+    await page.goto(`https://www.linkedin.com/in/${username}`, { waitUntil: 'networkidle2' });
+    // Generate PDF
+    const pdf = await page.pdf({ format: 'A4' });
+    await browser.close();
+    return pdf;
+}
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
