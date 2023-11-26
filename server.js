60b127965f3e1a7c4cdaa66d52a9cff548695e40
sid <sid@sids-Air.lan>
2023-11-25 18:16:13 -0800

    updated server.js file only

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
 const port = 3010;
+
+    
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -22,17 +25,18 @@ app.post('/download-profile', async (req, res) => {
 });
 
 async function generateLinkedInPDF(username) {
-    const browser = await puppeteer.launch({ headless: "new" }); 
+    // const browser = await puppeteer.launch({ headless: "new" }); 
+    // console.log('Browser executable path:', browser.executablePath());
     //const browser = await puppeteer.launch({ headless: true });
-    //const browser = await puppeteer.launch({ headless: false }); // Set headless: false for debugging
+    const browser = await puppeteer.launch({ headless: false }); // Set headless: false for debugging
     const page = await browser.newPage();
     const linkedinEmail = process.env.LINKEDIN_EMAIL || 'girishbisane9423@gmail.com'; // Replace with your email
     const linkedinPassword = process.env.LINKEDIN_PASSWORD || 'Hesoyam@9096'; // Replace with your password
     // Navigate to LinkedIn's login page
     await page.goto('https://www.linkedin.com/login');
     // Fill in the login form - make sure to use environment variables or another secure method to store credentials
-    await page.type('#username', 'yourLinkedInEmail', { delay: 30 });
-    await page.type('#password', 'yourLinkedInPassword', { delay: 30 });
+    await page.type('#username', 'girishbisane9423@gmail.com', { delay: 30 });
+    await page.type('#password', 'Hesoyam@9096', { delay: 30 });
 
     // Click the login button
     await page.click('[type="submit"]');
@@ -48,3 +52,21 @@ async function generateLinkedInPDF(username) {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+app.post('/generate-linkedin-pdf', async (req, res) => {
+    const username = req.body.username;
+    if (!username) {
+        return res.status(400).send('Username is required');
+    }
+
+    try {
+        const pdfBuffer = await generateLinkedInPDF(username);
+        res.setHeader('Content-Disposition', `attachment; filename=${username}_linkedin_profile.pdf`);
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(pdfBuffer);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error generating PDF');
+    }
+});
+
